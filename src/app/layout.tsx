35d929952d807629c3c871d9abdd6376075b1bda
Aniket-508 <pawaraniket508@gmail.com>
2026-01-21 19:12:09 +0530

    feat: add Clarity tracking script and update .env.example with CLARITY_ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "../../styles/global.css";
 import { Metadata, Viewport } from "next";
 
@@ -19,6 +20,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="clarity-tracking" strategy="afterInteractive">
+          {`
+            <script type="text/javascript">
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${process.env.CLARITY_ID}");
+            </script>
+          `}
+        </Script>
+      </head>
       <body className="bg-background">{children}</body>
     </html>
   );
